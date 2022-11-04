03a073d4c87004845d88d99e6008ead6ed0739af
Oriol Palacios <orioloriolpalacios@outlook.com>
2022-11-04 18:18:38 -0500

    Clase de clientes y módulos a excepción de el "listar las ventas de un cliente determinado", dado que aún no hay CVenta

--- LibreriaClases/CCliente.cs
+++ LibreriaClases/CCliente.cs
@@ -1,7 +1,55 @@
-﻿namespace LibreriaClases
+﻿using System.Collections;
+namespace LibreriaClases
 {
     public class CCliente
     {
+        #region Campos privados
+        private string _id;
+        private string _nombre;
+        private string _direccion;
+        #endregion
+        #region Getters & Setters
+        public string Id { 
+            get => _id; 
+            set
+            {
+                if (value.Length == 6)
+                {
+                    string[] StringDni = value.Split("");
+                    int[] IntsDni = StringDni.Select(k => int.Parse(k)).ToArray();
+                    _id = value;
+                }
+                else
+                    Console.WriteLine("El DNI está compuesto por 8 digitos");
+            }
+        }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Direccion { get => _direccion; set => _direccion = value; }
+        #endregion
+        #region Constructor
+        public CCliente(string id, string nombre, string direccion)
+        {
+            Id = id;
+            Nombre = nombre;
+            Direccion = direccion;
+        }
+        #endregion
+        #region Métodos
+        public void Mostrar()
+        {
+            Console.WriteLine("DNI: " + Id +"\tNombre: " + Nombre + "\tDireccion: " + Direccion);
+        }
+        public static void ListarClientes(ArrayList arr)
+        {
+            foreach (object k in arr)
+            {
+                if (k is CCliente)
+                    // Usando casting para tomar "k" como una instancia de CCliente en vez de un Obejct
+                    ((CCliente)k).Mostrar();
+            }
+        }
+
+        #endregion
 
     }
 }
--- ProyectoAlgoritmos1/Program.cs
+++ ProyectoAlgoritmos1/Program.cs
@@ -1,4 +1,30 @@
 ﻿// Proyecto de algoritmos y estructuras de datos
 // Usar la librería de clases
 using LibreriaClases;
+using System.Collections;
+// Declarar el ArrayList que contendrá todos los clientes de la app
+ArrayList Clientes = new();
+#region LOS 20 CLIENTES POR DEFECTO
+Clientes.Add(new CCliente("95961212", "Elías Vicente Carballo", "3467 Tawny Terrace"));
+Clientes.Add(new CCliente("36192134", "Marcos Gras Lastra", "837 Middle Diversion"));
+Clientes.Add(new CCliente("85586251", "Marcial Rivero Grau", "4430 Quaking Anchor Ramp"));
+Clientes.Add(new CCliente("85143178", "Jose Francisco Jáuregui Cortés", "6913 Golden Cider Park"));
+Clientes.Add(new CCliente("45686143", "Ofelia Zabaleta Miralles", "7712 Jagged Pine Passage"));
+Clientes.Add(new CCliente("82784817", "África Soledad Ruiz Piñol", "3302 Colonial Dale Meadow"));
+Clientes.Add(new CCliente("01278535", "Erasmo Mauricio Ortuño Ordóñez", "8773 Clear View"));
+Clientes.Add(new CCliente("34480959", "Cristian Herrero Puente", "9306 Amber Inlet"));
+Clientes.Add(new CCliente("05196068", "Eustaquio Yáñez-Ballesteros", "750 Cotton Highway"));
+Clientes.Add(new CCliente("97218107", "Joaquina Milagros Zamora Saez", "3775 Cinder Autoroute"));
+Clientes.Add(new CCliente("64093986", "Alejandra Jove Calleja", "9947 Little Range"));
+Clientes.Add(new CCliente("90773924", "Amelia Casandra Vizcaíno Amat", "8896 Grand Shadow Farm"));
+Clientes.Add(new CCliente("26620115", "Javier Pedro Azcona", "3989 Burning Falls"));
+Clientes.Add(new CCliente("50343623", "Juan Francisco Robles Escalona", "8592 Honey Timber Woods"));
+Clientes.Add(new CCliente("11423917", "Jacobo de Perera", "5299 High End"));
+Clientes.Add(new CCliente("67432981", "María José Villena Pinedo", "4942 Harvest Apple Plaza"));
+Clientes.Add(new CCliente("55852593", "Hortensia Elena Barco Palma", "98 Emerald Lane"));
+Clientes.Add(new CCliente("42691085", "Osvaldo Menéndez Alcalá", "8796 Pleasant Arbor"));
+Clientes.Add(new CCliente("072516", "Jose Francisco Galvez Alberola", "8011 Blue Grove Highlands"));
+Clientes.Add(new CCliente("226717", "Leyre Romero Carrión", "7686 Dusty Corner"));
+#endregion
+
 
