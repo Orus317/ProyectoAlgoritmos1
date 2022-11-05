e757eb57d3495c1bdff3958074e0d9eb0a1ee10f
Oriol Palacios <orioloriolpalacios@outlook.com>
2022-11-05 12:21:48 -0500

    Cproducto y cregistroVenta

--- LibreriaClases/CProducto.cs
+++ LibreriaClases/CProducto.cs
@@ -1,7 +1,68 @@
-﻿namespace LibreriaClases
-{
-    public class CProducto
-    {
+﻿// using System;
+using System.Collections;
 
+namespace LibreriaClases
+{
+    public class CProducto{
+        private int _IdProducto;
+        private string _Descripcion;
+        private string _Tipo;
+        private string _UnidadMedida;
+        private int _Stock;
+        private float _PrecioUnitario;
+
+        // Getters and setters
+        public int IdProducto{
+            get => _IdProducto; set => _IdProducto = value;
+        }
+
+        public string Descripcion{
+            get => _Descripcion; set => _Descripcion = value;
+        }
+
+        public string Tipo{
+            get => _Tipo; set => _Tipo = value;
+        }
+
+        public string UnidadMedida{
+            get => _UnidadMedida; set => _UnidadMedida= value;
+        }
+
+
+        public int Stock{
+            get => _Stock; set => _Stock = value;
+        }
+
+        public float PrecioUnitario{
+            get => _PrecioUnitario; set => _PrecioUnitario = value;
+        }
+        
+        // Constructor
+        public CProducto(int idproducto, string descripcion, string tipo, string unidadmedida, int stock, float preciounitario){
+            IdProducto = idproducto;
+            Descripcion = descripcion;
+            Tipo = tipo;
+            UnidadMedida = unidadmedida;
+            Stock = stock;
+            PrecioUnitario = preciounitario;
+        }
+        
+        public static void ListarProductos(ArrayList arr){
+            foreach (object k in arr){
+                if (k is CProducto) 
+                    ((CProducto)k).Mostrar();
+            }
+        }
+
+
+
+        public void Mostrar(){
+            Console.WriteLine("Identificardor: " + IdProducto);
+            Console.WriteLine("Descripcción: " + Descripcion);
+            Console.WriteLine("Tipo: " + Tipo);
+            Console.WriteLine("UnidadMedida: " + UnidadMedida);
+            Console.WriteLine("Precio unitario: " + PrecioUnitario);
+        }
     }
 }
+
--- LibreriaClases/CRegistroVenta.cs
+++ LibreriaClases/CRegistroVenta.cs
@@ -1,7 +1,99 @@
-﻿namespace LibreriaClases
-{
-    public class CRegistroVenta
-    {
+﻿// using System;
+using System.Collections;
 
+namespace LibreriaClases
+{
+    public class CRegistroVentas
+    {
+        // Atributos
+        private int _IdVenta;
+        private int _NroDocVenta;
+        private string _Fecha;
+        private string _IdCliente;
+        private int _IdProducto;
+        private int _Cantidad;
+        private float _PrecioUnitario;
+
+        // Getters and setters
+
+        public int IdVenta
+        {
+            get => _IdVenta; set => _IdVenta = value;
+        }
+
+        public int NroDocVenta
+        {
+            get => _NroDocVenta; set => _NroDocVenta = value;
+        }
+
+        public string Fecha
+        {
+            get => _Fecha; set => _Fecha = value;
+        }
+
+        public string IdCliente
+        {
+            get => _IdCliente; set
+            {
+                if (value.Length == 6)
+                {
+                    string[] StringDni = value.Split("");
+                    int[] IntsDni = StringDni.Select(k => int.Parse(k)).ToArray();
+                    _IdCliente = value;
+                }
+                else
+                    Console.WriteLine("El DNI está compuesto por 8 digitos");
+            }
+        }
+
+        public int IdProducto
+        {
+            get => _IdProducto; set => _IdProducto = value;
+        }
+
+        public int Cantidad
+        {
+            get => _Cantidad; set => _Cantidad = value;
+        }
+
+
+        public float PrecioUnitario
+        {
+            get => _PrecioUnitario; set => _PrecioUnitario = value;
+        }
+
+        // Constructor
+        public CRegistroVentas(int id_venta, int nro_docventa, string fecha, string id_cliente, int id_producto, int cantidad, float precio_unitario)
+        {
+            IdVenta = id_venta;
+            NroDocVenta = nro_docventa;
+            Fecha = fecha;
+            IdCliente = id_cliente;
+            IdProducto = id_producto;
+            Cantidad = cantidad;
+            PrecioUnitario = precio_unitario;
+        }
+
+        // Listar 
+        public static void ListarRegistroVentas(ArrayList arr)
+        {
+            foreach (object k in arr)
+            {
+                if (k is CRegistroVentas)
+                    ((CProducto)k).Mostrar();
+            }
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Identificador de venta:" + IdVenta);
+            Console.WriteLine("Número de venta: " + NroDocVenta);
+            Console.WriteLine("Fecha: " + Fecha);
+            Console.WriteLine("DNI del cliente: " + IdCliente);
+            Console.WriteLine("Identificador del producto: " + IdProducto);
+            Console.WriteLine("Cantidad: " + Cantidad);
+            Console.WriteLine("Precio Unitario: " + PrecioUnitario);
+        }
     }
 }
+
