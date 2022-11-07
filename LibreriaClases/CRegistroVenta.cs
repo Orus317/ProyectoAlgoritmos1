3734ff6aa222f23389be31069350b7831d318d4d
ianlw <92705301+ianlw@users.noreply.github.com>
2022-11-07 18:04:03 -0500

    Update CRegistroVenta.cs

--- LibreriaClases/CRegistroVenta.cs
+++ LibreriaClases/CRegistroVenta.cs
@@ -78,13 +78,16 @@ namespace LibreriaClases
                 }
             }
         }
+        // Calcular el dinero recaudado en una determinada fecha
         public static void fecha_vendida(ArrayList Lista, string fecha)
         {
             double a = 0;
+             // Recorrer la Lista
             for (int i = 0; i < Lista.Count; i++)
             {
                 if ((Lista[i] as CRegistroVentas).Fecha == fecha)
                 {
+                    // Calcular el dinero recaudaddo
                     a += (Lista[i] as CRegistroVentas).Cantidad * (Lista[i] as CRegistroVentas).PrecioUnitario;
                 }
             }
@@ -93,21 +96,25 @@ namespace LibreriaClases
             Console.WriteLine("-------------------------------");
             Console.WriteLine(a);
         }
+        
+        // Hallar el producto más vendido
         public static void producto_mas_vendido(ArrayList Lista, ArrayList Lista2)
         {
+            // Variables auxiliares
             int mas_vendido = 0;
             string id_mas_vendido = "";
-            string descripcion_mas_vendido = "";
-
+            string descripcion_mas_vendido = ""; 
+            // Recorremos la lista
             for (int i = 0; i < Lista.Count; i++)
             {
                 int contador = 0;
                 string producto = (Lista[i] as CRegistroVentas).IdProducto;
-
+                // Encontramos la repetición del producto
                 for (int k = 0; k < Lista.Count; k++)
                 {
                     if ((Lista[k] as CRegistroVentas).IdProducto == producto)
                     {
+                        // Sumamos la cantidad unidades vendidas 
                         contador += (Lista[k] as CRegistroVentas).Cantidad;
                     }
 
@@ -133,6 +140,8 @@ namespace LibreriaClases
             Console.WriteLine("Descripción: " + descripcion_mas_vendido);
             Console.WriteLine("Unidades vendidas: " + mas_vendido);
         }
+        
+        // Listado de las ventas de una determinada fecha
         public static void listarVentasPorFecha(ArrayList RegistroVentas, string fecha)
         {
             fecha = ValidarFecha(fecha);
@@ -142,14 +151,19 @@ namespace LibreriaClases
                     ((CRegistroVentas)Venta).Mostrar();
             }
         }
+        // Validar fecha  
         private static string ValidarFecha(string fecha)
         {
+            // Separar el día mes y año 
             string[] digitosFechaStr = fecha.Split("/");
             int[] digitosFechaInt = digitosFechaStr.Select(k => int.Parse(k)).ToArray();
+            // Verificar que sea un día correcto
             if (digitosFechaInt[0] > 0 && digitosFechaInt[0] < 32)
             {
+                // Verificar que sea un mes correcto
                 if (digitosFechaInt[1] > 0 && digitosFechaInt[1] < 13)
                 {
+                    // Verificar que sea un año correcto
                     if (digitosFechaInt[2] > 0)
                     {
                         return fecha;
@@ -159,23 +173,26 @@ namespace LibreriaClases
             Console.WriteLine("La fecha no es válida");
             return "NE";
         }
-        // Pregunta 8: Lista de ventas de un determinado cliente 
+        
+        // Lista de ventas de un determinado cliente 
         public static void listarVentasCliente(ArrayList Lista, string DNICliente)
         {
             int a = 0;
             for (int i = 0; i < Lista.Count; i++)
             {
+                // Hallamos al cliente
                 if ((Lista[i] as CRegistroVentas).IdCliente == DNICliente)
                 {
                     Console.WriteLine("\n------------------------");
                     Console.WriteLine("   Venta número " + ++a);
                     Console.WriteLine("------------------------");
                     (Lista[i] as CRegistroVentas).Mostrar();
+                    // Calculamos el subtotal
                     double subtotal = (Lista[i] as CRegistroVentas).Cantidad * (Lista[i] as CRegistroVentas).PrecioUnitario;
                     Console.WriteLine("Subtotal: " + subtotal);
                 }
             }
-            if (a == 0)
+            if (a == 0) // Si es igual a cero significa que al cliente no se le registro ninguna venta
                 Console.WriteLine("No se encontraron ventas a " + DNICliente);
         }
         public void Mostrar()
